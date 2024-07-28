637183787c3026c23dd973dd8ab61b72f6b8ae47
kaaroll99 <karol.wykrota@hotmail.com>
2024-07-28 14:17:09 +0200

    database update

--- database/data_import.py
+++ database/data_import.py
@@ -1,35 +1,20 @@
-import sqlite3
+import csv
 import mysql.connector
 import config
+from config import bot, db
 
 
 def import_database():
     token_file = config.load_yml('token.yml')
 
-    # Połączenie z bazą SQLite
-    sqlite_conn = sqlite3.connect('guilds.db')
-    sqlite_cursor = sqlite_conn.cursor()
+    # Otworzenie pliku CSV
+    with open('guilds.csv', 'r') as csv_file: # Zmień 'guilds.csv' na rzeczywistą nazwę pliku
+        csv_reader = csv.reader(csv_file)
 
-    # Połączenie z bazą MySQL
-    mysql_conn = mysql.connector.connect(
-        host=token_file['db_host'],
-        user=token_file['db_login'],
-        password=token_file['db_pass'],
-        database=token_file['db_name']
-    )
-    mysql_cursor = mysql_conn.cursor()
-
-    # Pobranie danych z SQLite
-    sqlite_cursor.execute("SELECT * FROM guilds")
-    rows = sqlite_cursor.fetchall()
-
-    # Wstawienie danych do MySQL
-    for row in rows:
-        mysql_cursor.execute("INSERT INTO guilds VALUES (%s, %s, %s)", row)
-        print(row)
-
-    mysql_conn.commit()
-
-    # Zamknięcie połączeń
-    sqlite_conn.close()
-    mysql_conn.close()
+        db.connect()
+        # Iteracja po wierszach CSV (pomijając nagłówek, jeśli istnieje)
+        next(csv_reader, None)
+        for row in csv_reader:
+            db.create(model.guilds_class(f"guilds"), {"server": row[0], "role": row[1]})
+            print(row)
+        db.close()
