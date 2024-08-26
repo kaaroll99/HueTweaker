bb4dbe93d0b1c7f5904b0a17771d2ac9caa45986
kaaroll99 <karol.wykrota@hotmail.com>
2024-08-26 23:48:15 +0200

    main change

--- main.py
+++ main.py
@@ -7,23 +7,6 @@ from config import db, bot
 
 config_file = config.load_yml('config.yml')
 token_file = config.load_yml('token.yml')
-cogs = ['help', 'set', 'remove', 'check', 'force', 'setup', 'joinListener', 'vote', 'select']
-dev_cogs = ['dev']
-
-
-@bot.event
-async def on_ready():
-    try:
-        cmds = []
-        synced = await bot.tree.sync()
-        for command in synced:
-            cmds.append(command.name)
-        bot.remove_command('help')
-
-        logging.info(f"Cogs ({len(cogs)}): " + ", ".join([f'{cog}' for cog in cogs]))
-        logging.info(f"Active commands ({len(cmds)}): " + ", ".join([f'{cmd}' for cmd in cmds]))
-    except Exception as e:
-        print(e)
 
 
 async def main():
