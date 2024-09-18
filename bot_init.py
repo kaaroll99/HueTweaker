373fd41bc17c744582c15d36452f73816a08b8cc
kaaroll99 <karol.wykrota@hotmail.com>
2024-09-18 13:27:36 +0200

    http 429-7

--- bot_init.py
+++ bot_init.py
@@ -1,5 +1,6 @@
-import logging
 import asyncio
+import logging
+
 import discord
 from discord import app_commands, Locale
 from discord.ext import commands
@@ -16,18 +17,23 @@ class MyBot(commands.AutoShardedBot):
 
     async def setup_hook(self):
         cogs = ['help', 'set', 'remove', 'check', 'force', 'setup', 'joinListener', 'vote', 'select']
+        await asyncio.sleep(1)
         logging.info("Loading extensions: " + ", ".join(cogs))
         for i, cog in enumerate(cogs, start=1):
             try:
+                await asyncio.sleep(1)
+                logging.info(f"Loading {cog} cog ...")
                 await self.load_extension(f"cogs.{cog}")
             except Exception as e:
                 logging.error(f"Failed to load extension {cog}: {e}")
         logging.info("Loading of extensions completed")
         logging.info("Loading translator ...")
         await self.tree.set_translator(MyTranslator(self))
+        await asyncio.sleep(1)
         logging.info("Loading translator completed")
         logging.info("Command tree synchronization ...")
         await self.tree.sync()
+        await asyncio.sleep(2)
         logging.info("Command tree synchronization completed")
 
 class MyTranslator(app_commands.Translator):
