5bd82695a278808fec316a4747f80cd42d1320b8
kaaroll99 <karol.wykrota@hotmail.com>
2024-09-21 00:09:01 +0200

    config.py

--- config.py
+++ config.py
@@ -7,9 +7,8 @@ config_file = load_yml('assets/config.yml')
 if token_file.get('system', None) == 'dev':
     db = database.Database(url=f"sqlite:///assets/guilds.db")
 else:
-    db = database.Database(url=f"sqlite:///assets/guilds.db")
-    # db = database.Database(
-    #     url=f"mysql+pymysql://{token_file['db_login']}:{token_file['db_pass']}@{token_file['db_host']}/{token_file['db_name']}")
+    db = database.Database(
+        url=f"mysql+pymysql://{token_file['db_login']}:{token_file['db_pass']}@{token_file['db_host']}/{token_file['db_name']}")
 
 logger = setup_logger()
 logger.info("Log file has been created.")
