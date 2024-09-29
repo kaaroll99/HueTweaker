0a1049d97d8b1f72dfe6d5bd2334b7930569aac7
kaaroll99 <karol.wykrota@hotmail.com>
2024-09-29 23:57:50 +0200

    bot.py old files

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-import os
-
-from dotenv import load_dotenv
-
-from database import database
-
-load_dotenv(".env")
-
-if os.getenv('system', None) == 'DEV':
-    pass
-else:
-    db = database.Database(
-        url=f"mysql+pymysql://{os.getenv('db_login')}:{os.getenv('db_pass')}@{os.getenv('db_host')}/{os.getenv('db_name')}")
-
-# logger.info("Log file has been created.")
+# import os
+#
+# from dotenv import load_dotenv
+#
+# from database import database
+#
+# load_dotenv(".env")
+#
+# if os.getenv('system', None) == 'DEV':
+#     pass
+# else:
+#     db = database.Database(
+#         url=f"mysql+pymysql://{os.getenv('db_login')}:{os.getenv('db_pass')}@{os.getenv('db_host')}/{os.getenv('db_name')}")
+#
+# # logger.info("Log file has been created.")
