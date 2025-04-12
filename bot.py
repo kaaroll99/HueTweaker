837ad77ea28c15897af67a61bef60bc492c54cdb
kaaroll99 <karol.wykrota@hotmail.com>
2025-04-12 23:28:41 +0200

    move api post requests

--- bot.py
+++ bot.py
@@ -1,19 +1,15 @@
 import asyncio
 import datetime
 import logging
-import os
 
 import discord
-import requests
 from discord import Locale
 from discord.ext import commands, tasks
-from msgspec import json
 
 from database import database
 from utils.console_logger import setup_logger
-from utils.data_loader import load_json, load_yml
-
-import aiohttp
+from utils.data_loader import load_yml
+from utils.stats_api import api_request
 
 setup_logger()
 logger = logging.getLogger(__name__)
@@ -30,28 +26,12 @@ else:
 
 cmd_messages = load_yml('assets/messages.yml')
 
-async def post_data(url: str, headers: dict, data: dict, message: str = "server count") -> dict:
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=headers, json=data, timeout=aiohttp.ClientTimeout(total=10)) as response:
-                if response.status == 200:
-                    logging.info(f'Successfully posted {message} to {url}')
-                    return {"status": response.status, "success": True}
-                else:
-                    logging.warning(f'Failed to post {message} to {url}: {response.status} {response.reason}')
-                    return {"status": response.status, "success": False, "reason": response.reason}
-    except asyncio.TimeoutError:
-        logging.error(f"Request to {url} timed out")
-        return {"status": None, "success": False, "reason": "Timeout"}
-    except Exception as e:
-        logging.error(f'Error posting {message} to {url}: {e}', exc_info=True)
-        return {"status": None, "success": False, "reason": str(e)}
 
 class MyBot(commands.AutoShardedBot):
-    update_times = [
-        datetime.time(hour=1, minute=0, tzinfo=datetime.timezone(datetime.timedelta(hours=1), 'CET')),
-        datetime.time(hour=12, minute=0, tzinfo=datetime.timezone(datetime.timedelta(hours=1), 'CET'))
-    ]
+    # update_times = [
+    #     datetime.time(hour=1, minute=0, tzinfo=datetime.timezone(datetime.timedelta(hours=0), 'CET')),
+    #     datetime.time(hour=23, minute=21, tzinfo=datetime.timezone(datetime.timedelta(hours=0), 'CET'))
+    # ]
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.default_locale = Locale.american_english
@@ -66,58 +46,15 @@ class MyBot(commands.AutoShardedBot):
             except Exception as e:
                 logger.error(f"Failed to load extension {cog}: {e}")
 
-    @tasks.loop(time=update_times)
+    @tasks.loop(hours=12)
     async def update_stats_task(self):
-        # Update bot stats top.gg
-        url = f'https://top.gg/api/bots/1209187999934578738/stats'
         server_count = len(self.guilds)
-        headers = {
-            'Authorization': token_file['TOP_GG_TOKEN'],
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'server_count': server_count,
-            'shard_count': 2
-        }
-        await post_data(url, headers, data, message="stats to Top.gg")
-        
-        # Update bot stats discordlist.gg
-        url = f'https://api.discordlist.gg/v0/bots/1209187999934578738/guilds'
-        server_count = len(self.guilds)
-        headers = {
-            'Authorization': f"Bearer {token_file['DISCORDLIST_GG_TOKEN']}",
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-        data = {
-            'count': server_count
-        }
-        await post_data(url, headers, data, message="stats to discordlist.gg")
-        
-        # Update bot stats discordbotlist.com
-        url = "https://discordbotlist.com/api/v1/bots/1209187999934578738/stats"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": token_file['DISCORDBOTLIST_TOKEN']
-        }
-        data = {
-            "users": sum(guild.member_count for guild in self.guilds), 
-            "guilds": len(self.guilds)
-        }
-        await post_data(url, headers, data, message="stats to discordbotlist.com")
-
-        # Update commands list discordbotlist.com
-        url = f"https://discordbotlist.com/api/v1/bots/1209187999934578738/commands"
-        json_payload = load_json("assets/commands_list.json")
-        headers = {
-            "Authorization": token_file['DISCORDBOTLIST_TOKEN'],
-            "Content-Type": "application/json"
-        }
-        await post_data(url, headers, json_payload, message="command list")
+        user_count = sum(guild.member_count for guild in self.guilds)
+        await api_request(server_count, user_count)
 
     @update_stats_task.before_loop
     async def before_status_task(self) -> None:
         await self.wait_until_ready()
-
 
     async def setup_hook(self) -> None:
         await self.load_cogs()
@@ -132,17 +69,14 @@ class MyBot(commands.AutoShardedBot):
 
         self.update_stats_task.start()
 
-
     async def on_ready(self) -> None:
         self.remove_command('help')
         logging.info(20 * '=' + " Bot is ready. " + 20 * "=")
-
 
     @staticmethod
     async def on_socket_response(msg) -> None:
         if msg.get('t') == 'RESUMED':
             logging.info('Shard connection resumed.')
-
 
     @staticmethod
     async def on_shard_disconnect(shard_id) -> None:
