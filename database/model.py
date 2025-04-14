5f04836b30be6a67a5948e6a60a750c1e47645ba
kaaroll99 <karol.wykrota@hotmail.com>
2025-04-14 13:48:52 +0200

    form for edit

--- database/model.py
+++ database/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger
+from sqlalchemy import Column, BigInteger, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -15,3 +15,24 @@ class Guilds(Base):
 def guilds_class(table_name):
     Guilds.__tablename__ = table_name
     return Guilds
+
+
+class Select(Base):
+    __tablename__ = 'select'
+    __table_args__ = {'extend_existing': True}
+    server_id = Column(Integer, primary_key=True, nullable=False)
+    hex_1 = Column(Text)
+    hex_2 = Column(Text)
+    hex_3 = Column(Text)
+    hex_4 = Column(Text)
+    hex_5 = Column(Text)
+    hex_6 = Column(Text)
+    hex_7 = Column(Text)
+    hex_8 = Column(Text)
+    hex_9 = Column(Text)
+    hex_10 = Column(Text)
+    
+    
+def select_class(table_name):
+    Select.__tablename__ = table_name
+    return Select
